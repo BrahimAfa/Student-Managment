18f1052949267785b2533ed5f8d423de7838dbd9
Brahim <brahimafassy@gmail.com>
2020-12-21 06:39:31 +0100

    Update : changed Folder Names WinFormApp => ApplicationUI

--- ApplicationUI/Services/EtudiantService.cs
+++ ApplicationUI/Services/EtudiantService.cs
@@ -5,12 +5,18 @@ using System.Text;
 using System.Threading.Tasks;
 using Core.Database;
 using ApplicationUI.Etudiant;
+using System.Data.SqlClient;
 namespace ApplicationUI.Services
 {
   class EtudiantService : IDAO<EtudiantModel>
   {
+    // public SqlCommand sqlcommand { get; set; }
+    public EtudiantService()
+    {
+    }
     public bool delete(int id)
     {
+      // SqlCommand sqlcommand = new SqlCommand("query",Connection.sqlConnection)
       throw new NotImplementedException();
     }
 
