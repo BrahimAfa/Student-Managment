63cb571b1199342f08d06fe251d5a1b40cc1745d
Brahim <brahimafassy@gmail.com>
2021-02-21 19:17:22 +0100

    added plugins to run rdlc reports

--- StudentManagementBeta/Properties/Settings.Designer.cs
+++ StudentManagementBeta/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace StudentManagementBeta.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.8.1.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -31,6 +31,18 @@ namespace StudentManagementBeta.Properties {
         public string GestionEtudiantConnectionString {
             get {
                 return ((string)(this["GestionEtudiantConnectionString"]));
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string Setting {
+            get {
+                return ((string)(this["Setting"]));
+            }
+            set {
+                this["Setting"] = value;
             }
         }
     }
